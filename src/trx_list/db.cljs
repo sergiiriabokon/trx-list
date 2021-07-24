7334578fdec9ab1acfd30f4dcce5a2fb718ae42e
Serhii <sergiiriabokon@gmail.com>
2021-07-24 15:38:35 +0300

    currency changed to UAH

--- src/trx_list/db.cljs
+++ src/trx_list/db.cljs
@@ -7,18 +7,18 @@
                :to "Brian Gold"
                :to-account "3332-2232-3444-2378"
                :amount 234.53
-               :currency "USD"}
+               :currency "UAH"}
               
               {:from "Jean Apple"
                :from-account "1234-1423-5333-2222"
                :to "Giovanni Mol"
                :to-account "1132-2982-3004-2008"
                :amount 300.0
-               :currency "USD"}
+               :currency "UAH"}
               
               {:from "Josh Big"
                :from-account "1884-1403-5243-3372"
                :to "Greg Sonwalker"
                :to-account "9152-2080-3414-8008"
                :amount 730.0
-               :currency "USD"}]})
+               :currency "UAH"}]})
